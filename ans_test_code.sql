3b98f25f85012c736f100f566c0d69817c1a3591
Kevin Lidh <47402646+kgdevops@users.noreply.github.com>
2020-02-27 09:40:05 -0700

    Adding a SQL that can be re-run

---
+++ ans_test_code.sql
@@ -0,0 +1,12 @@
+DECLARE
+  cursor c1 is
+    select max(ANS_DATA) from kglidh.ansible_test;
+  max_data number;
+BEGIN
+  open c1;
+  fetch c1 into max_data;
+  max_data := max_data+1;
+  insert into kglidh.ansible_test values (sysdate,max_data,'From PLSQL script');
+  close c1;
+END;
+/
